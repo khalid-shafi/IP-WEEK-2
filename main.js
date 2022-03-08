c780b1fd5f4b45076e26acdeaac1766ce4d073c9
khalid-shafi <khalidshafi2001@gmail.com>
2022-03-08 15:29:55 +0300

    added an event listner

--- main.js
+++ main.js
@@ -1,127 +1,41 @@
-let months =[
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ];
 
-      const maleAkan = [
+   const maleAkan = [
         "Kwasi",
         "Kwadwo",
         "Kwabena",
         "Kwaku",
         "yaw",
         "kofi",
-        "kwame",
+        "kwame"
       ];
       const femaleAkan =[
         "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
       ];
-      const month = document.getElementById("month");
-      const day = document.getElementById("day");
-      const btn = document.getElementById("btn");
-      const year = document.getElementById("year");
-      const gender = document.getElementById("gender");
-      const dayResult = document.querySelector("dayResult");
-      const monthResult = document.querySelector("monthResult");
-      const name = document.querySelector("name");
-
-    
-
-      btn.addEventListener("click", function () {
-        const MM = month.value;
-        const DD = day.value;
-        if (DD < 1 || DD > 31) {
-          dayResult.textContent = "invalid day";
-          return;
-        }
-        if (MM < 1 || MM > 12) {
-          monthResult.textContent = "invalid month";
-          return;
-        }
-        
-        var date1 = months[MM-1] + " " + DD + " ," + year.value;
-        var New = new Date(date1);
-        var Day =  New.getDay();
-
-        console.log (date1)
-
-
-        if (gender.value == "female") {
-            switch (Day) {
-              case 0:
-              //   name.textContent = femaleAkan[0];
-              alert(" Your akan name is "+ femaleAkan[0]);
-                break;
-              case 1:
-              //   name.textContent = femaleAkan[1];
-              alert( "Your akan name is " + femaleAkan[1]);
-                break;
-              case 2:
-              //   name.textContent = femaleAkan[2];
-              alert(" Your akan name is " +femaleAkan[2]);
-                break;
-              case 3:
-              //   name.textContent = femaleAkan[3];
-              alert( " Your akan name is " +femaleAkan[3]);
-                break;
-              case 4:
-              //   name.textContent = femaleAkan[4];
-              alert( " Your akan name is " +femaleAkan[4]);
       
-                break;
-              case 5:
-              //   name.textContent = femaleAkan[5];
-              alert(femaleAkan[5]);
-                break;
-              case 6:
-              //   name.textContent = femaleAkan[6];
-              alert( " Your akan name is " +femaleAkan[6]);
-                break;
-              default:
-              //   name.textContent = "out of range";
-              alert("you are out of range");
-            }
-          } else  {
-            switch (Day) {
-              case 0:
-                // name.textContent = maleAkan[0];
-                alert( " Your akan name is " +maleAkan[0]);
-                break;
-              case 1:
-                // name.textContent = maleAkan[1];
-                alert(" Your akan name is " +maleAkan[1]);
-                break;
-              case 2:
-                // name.textContent = maleAkan[2];
-                alert(" Your akan name is " +maleAkan[2]);
-                break;
-              case 3:
-                // name.textContent = maleAkan[3];
-                alert(" Your akan name is " +maleAkan[3]);
-                break;
-              case 4:
-                // name.textContent = maleAkan[4];
-                alert(" Your akan name is " +maleAkan[4]);
-                break;
-              case 5:
-                // name.textContent = maleAkan[5];
-                alert(" Your akan name is " +maleAkan[5]);
-                break;
-              case 6:
-                // name.textContent = maleAkan[6];
-                alert(" Your akan name is " +maleAkan[6]);
-                break;
-              default:
-                name.textContent = "out of range";
-            }
+      const btn = document.getElementById("btn");
+      
+      
+      
+      btn.addEventListener("click", function (e) {
+        e.preventDefault();
+          const date = document.getElementById("date");
+          const gender = document.querySelector("input[type= 'radio']:checked");
+          const result = document.getElementById("result");
+
+          console.log(date);
+          let Akan = date.value;
+          let birthday = new Date(Akan);
+          let Day =  birthday.getDay();
+
+          if (gender.value  === "male"){
+              result.innerHTML = "your name is " + maleAkan[Day];
           }
+          if(gender.value === "female"){
+            result.innerHTML = femaleAkan[Day];
+          }
+          
+          
+          
+          
+          
         });
